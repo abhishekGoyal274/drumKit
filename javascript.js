28bf5922d70f1e366322e79b5017a09576cedd5c
Nipun Garg <77457267+ngarg2k2@users.noreply.github.com>
2021-10-08 09:59:51 +0530

    Enables sound for capital letters

--- javascript.js
+++ javascript.js
@@ -46,6 +46,34 @@ function makeSound(key) {
         case "l":
             var audio = new Audio("kick-bass.mp3");
             audio.play();
+            break;    
+        case "W":
+            var audio = new Audio("tom-1.mp3");
+            audio.play();
+            break;
+        case "A":
+            var audio = new Audio("tom-2.mp3");
+            audio.play();
+            break;
+        case "S":
+            var audio = new Audio("tom-3.mp3");
+            audio.play();
+            break;
+        case "D":
+            var audio = new Audio("tom-4.mp3");
+            audio.play();
+            break;
+        case "J":
+            var audio = new Audio("snare.mp3");
+            audio.play();
+            break;
+        case "K":
+            var audio = new Audio("crash.mp3");
+            audio.play();
+            break;
+        case "L":
+            var audio = new Audio("kick-bass.mp3");
+            audio.play();
             break;
 
         default:
